2a9e482eaf2b4629c150a8604433f452fb9f645b
01BARRETO1 <karbar_1990@hotmail.com>
2026-08-12 16:58:49 -0500

    Taller 02 - EJERCICIO CALCULADORA

--- src/com/krakedev/Calculadora.java
+++ src/com/krakedev/Calculadora.java
@@ -16,5 +16,39 @@ public class Calculadora {
 		
 		return resultado;
 	}
+	
+	//Función: multiplicar
+	
+	public double multiplicar(double valor1, double valor2) {
+		double result;
+		
+		result =valor1*valor2; 
+		return result;
+	}
+	
+	//Funcion dividir
+	
+	public double dividir(double dividiendo, double divisor) {
+		double result;
+		
+		result =dividiendo/divisor; 
+		return result;
+	}
+	
+	//Funcion Promediar
+	
+	public double promediar(double valor1, double valor2,  double valor3) {
+		double result;
+		
+		result =(valor1+valor2+valor3)/3;
+		return result;
+	}
+	
+	//Funcion mostarrResultado
+	
+	public void mostrarResultado() {
+		System.out.println("Ahoritas no joven, Salí al almuerzo. Regreso en 1 hora"); 
+	}
+	
 
 }
--- src/com/krakedev/TestCalculadora.java
+++ src/com/krakedev/TestCalculadora.java
@@ -13,7 +13,38 @@ public class TestCalculadora {
 		int r2=c1.restar(4, 2);
 		
 		System.out.println("Resultado resta: "+r2 );
+		
+		//Funcion multiplicar
+		Calculadora multiplicacion = new Calculadora();
+		
+		double m = multiplicacion.multiplicar(10, 5);
+		
+		System.out.println("La multiplicacion es : "+ m );
+		
+		//funcion Dividir
+		
+		Calculadora division=new Calculadora();
+		//Variable d ... en la variable guardamos el llamado (el cociente)
+		double d=division.dividir(10, 2);
+		//mostrar en consola
+		System.out.println("La division es: "+ d);
+		
+		//funcion promediar
+		
+		Calculadora promedio=new Calculadora();
+		
+		double p=promedio.promediar(10, 8, 9);
+		
+		System.out.println("El promedio es: "+p);
+		
+		//funcion mostrarResultado
+		
+		c1.mostrarResultado();
 
 	}
+	
+	
+	
+	
 
 }
